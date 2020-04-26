a5435234389ea07ab325eabb6c109368acc194e2
wangruigang <286526823@qq.com>
2020-04-26 09:53:31 +0800

    去除信息

--- config/config.default.js
+++ config/config.default.js
@@ -11,8 +11,8 @@
 exports.jpush = {
   app: true,
   agent: false,
-  appKey: '03f4851a41b05d3bf6d9f9fa',
-  masterSecret: '392b95f3c4857b0430c2550f',
-  appName: 'maaee-ann',
+  appKey: '',
+  masterSecret: '',
+  appName: '',
   isDebug: true,
 };
